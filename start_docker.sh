f7e482354738f817e87f067a29874a9ddb5d625b
QuantumPlank <QuantumPlank@outlook.com>
2023-09-26 16:39:35 -0600

    Fix docker script logic

--- start_docker.sh
+++ start_docker.sh
@@ -3,20 +3,20 @@
 WORK_DIR=$(pwd)
 IMAGE_NAME=u20melp
 
-
-if docker image inspect $IMAGE_NAME &>/dev/null; then
+if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
 	echo "Image doesn't exists locally"
 	echo "Building..."
 	cd $WORK_DIR/Docker
-	docker build . -t ubuntu_melpi
+	docker build . -t $IMAGE_NAME
 fi
+
 cd $WORK_DIR
 echo "Starting Docker Container"
 
 docker run \
 	-it \
 	--rm \
-	-v $(WORK_DIR)/work:/work \
+	-v $WORK_DIR/work:/work \
 	-v /etc/passwd:/etc/passwd:ro \
 	-v /etc/group:/etc/group:ro \
 	-u $(id -u ${USER}):$(id -g ${USER}) \
