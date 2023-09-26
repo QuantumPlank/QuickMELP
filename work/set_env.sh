755326c8ca96da05d5fe4cecfec5621429709513
QuantumPlank <QuantumPlank@outlook.com>
2023-09-26 16:47:04 -0600

    Fix environment script logic

--- work/set_env.sh
+++ work/set_env.sh
@@ -1,4 +1,4 @@
-if $1=="Full"; then
+if [ "$1" = "full" ]; then
 	git clone https://github.com/crosstool-ng/crosstool-ng.git
 	cd crosstool-ng
 	git checkout crosstool-ng-1.24.0
@@ -6,5 +6,6 @@ if $1=="Full"; then
 	./configure --prefix=$(pwd)
 	make
 	make install
+fi
 
 export PATH="${PATH}:/work/crosstool-ng/bin"
