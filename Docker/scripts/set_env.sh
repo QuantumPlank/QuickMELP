d1636f812ff584ee66ab067b0ea97c1b3cf1204a
QuantumPlank <quantumplank@outlook.com>
2024-03-23 21:14:47 -0600

    Handle beaglebone black

--- Docker/scripts/set_env.sh
+++ Docker/scripts/set_env.sh
@@ -13,16 +13,23 @@ WORK_DIR=$(pwd)
 CROSSTOOL_DIR=$WORK_DIR/crosstool-ng
 TOOLCHAIN_DIR=$WORK_DIR/x-tools
 
-# Targets
+# QEMU
 TGT_QEMU=arm-unknown-linux-gnueabi
 TGT_QEMU_BIN=$TOOLCHAIN_DIR/$TGT_QEMU/bin
 TGT_QEMU_ARCH=arm
+# RPI4
 TGT_RPI4=aarch64-rpi4-linux-gnu
 TGT_RPI4_BIN=$TOOLCHAIN_DIR/$TGT_RPI4/bin
 TGT_RPI4_ARCH=arm
+# OPI3
 TGT_OPI3=aarch64-opi3-linux-gnu
 TGT_OPI3_BIN=$TOOLCHAIN_DIR/$TGT_OPI3/bin
 TGT_OPI3_ARCH=arm
+# BBLACK
+TGT_BBLACK=arm-cortex_a8-linux-gnueabi
+TGT_BBLACK_BIN=$TOOLCHAIN_DIR/$TGT_BBLACK/bin
+TGT_BBLACK_ARCH=arm
+
 
 if [ -z $CROSSTOOL_VERSION ]; then
 	CROSSTOOL_VERSION="crosstool-ng-1.26.0"
@@ -54,6 +61,25 @@ case $1 in
 				cd $CROSSTOOL_DIR
 				bin/ct-ng distclean
 				bin/ct-ng $TGT_QEMU
+				sed -i 's+CT_LOCAL_TARBALLS_DIR="${HOME}/src"+CT_LOCAL_TARBALLS_DIR="'"$WORK_DIR"'/src"+g' .config
+				sed -i 's/CT_PREFIX_DIR_RO=y/# CT_PREFIX_DIR_RO is not set/g' .config
+				sed -i 's+CT_PREFIX_DIR="${CT_PREFIX:-${HOME}/+CT_PREFIX_DIR="${CT_PREFIX:-"'"$WORK_DIR"'"/+g' .config
+				bin/ct-ng build.$(nproc --all)
+				cd $WORK_DIR
+			else
+				echo "TOOLCHAIN already installed"
+			fi
+		else
+			echo "CROSSTOOL is not installed"
+		fi
+		;;
+	"setup_bblack")
+		append_path $CROSSTOOL_DIR
+		if [[ -d $CROSSTOOL_DIR ]]; then
+			if [[ ! -d $TGT_BBLACK_BIN ]]; then
+				cd $CROSSTOOL_DIR
+				bin/ct-ng distclean
+				bin/ct-ng $TGT_BBLACK
 				sed -i 's+CT_LOCAL_TARBALLS_DIR="${HOME}/src"+CT_LOCAL_TARBALLS_DIR="'"$WORK_DIR"'/src"+g' .config
 				sed -i 's/CT_PREFIX_DIR_RO=y/# CT_PREFIX_DIR_RO is not set/g' .config
 				sed -i 's+CT_PREFIX_DIR="${CT_PREFIX:-${HOME}/+CT_PREFIX_DIR="${CT_PREFIX:-"'"$WORK_DIR"'"/+g' .config
@@ -110,6 +136,15 @@ case $1 in
 			append_path $TGT_QEMU_BIN
 			export CROSS_COMPILE=$TGT_QEMU-
 			export ARCH=$TGT_QEMU_ARCH
+		else
+			echo "TOOLCHAIN is not installed"
+		fi
+		;;
+	"set_bblack")
+		if [[ -d $TGT_BBLACK_BIN ]]; then
+			append_path $TGT_BBLACK_BIN
+			export CROSS_COMPILE=$TGT_BBLACK-
+			export ARCH=$TGT_BBLACK_ARCH
 		else
 			echo "TOOLCHAIN is not installed"
 		fi
